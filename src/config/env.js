181e9252f4796a4629b66423b17b706266f13d39
AshwiniGoutam <ashwin@devwings.com>
2026-09-09 11:00:51 +0530

    latest code

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://symcure.myclientwebsite.com/api/v1';
-// const API_BASE_URL = 'https://api.symcure.com/api/v1/';
+// const API_BASE_URL = 'https://symcure.myclientwebsite.com/api/v1';
+const API_BASE_URL = 'https://api.symcure.com/api/v1/';
 
 export {API_BASE_URL};
